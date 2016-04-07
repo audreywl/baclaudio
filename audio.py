1d53c33ae1d7d841a7468642b3cafb4f4cafde1b
esundsmo <esundsmo@gmail.com>
2016-04-07 17:08:54 -0400

    got rid of a few unused libraries

--- audio.py
+++ audio.py
@@ -1,7 +1,4 @@
 """This is the development file for processing and playing back the audio. It is called from the visualization file using the current time stamp, and provides a value for bpm, mood (key), and where the timestamp is in relation to the beats."""
-import alsaaudio
-import audioop
-import wave
 import time
 import numpy
 import pygame.mixer
