5167ba7403a3411549622ba011dca1d1032ae08b
audreywl <audrey.lewis@students.olin.edu>
2016-05-04 20:55:05 -0400

    wrapper: now commented, nice, and smarter than ever!

--- wrapper.py
+++ wrapper.py
@@ -22,6 +22,7 @@ def run_analyses(song):
 	song.lyric_sentiment()
 
 def find_new_path():
+	"""Takes a user inputed path and checks if it's valid. If not, it calls itself again."""
 	path = raw_input('Please enter the path the files. For example "/home/audrey/Music/"')
 	if not os.path.isdir(path):
 		print 'Please enter the name of a valid path'
@@ -29,9 +30,11 @@ def find_new_path():
 	return path
 
 def find_directory():
+	"""Prompts the user to identify the directory where the audio and lyric files are stored (first assumes the working directory is the one)"""
 	yes = ['yes','y', 'ye', '']
 	no = ['no','n']
-	choice = raw_input('Where can I find the lrc and mp3 file for this song? In this folder?(Y/n)').lower()
+	prompt = 'Where can I find the lrc and mp3 file for this song? In the folder below?(Y/n)' + '\n' +os.getcwd() + '\n'
+	choice = raw_input(prompt).lower()
 	if choice in yes:
 		path = os.getcwd()
 	elif choice in no:
@@ -41,7 +44,25 @@ def find_directory():
    		find_directory()
    	return path
 
+def file_choice(display_files, type_of_file):
+	"""Prompts the user to choose a file from a list. If they choose none of the above, it returns None."""
+	prompt = 'Which of these files contains the {}? Please enter its number.'.format(type_of_file)
+	try:
+		choice = int(raw_input(prompt))
+		if choice > len(display_files):
+			raise ValueError
+	except ValueError:
+		print 'Please enter one of the file numbers'
+		return file_choice(display_files, type_of_file)
+	if choice == len(display_files):
+		return None
+	else:
+		return choice-1
+		
+
 def find_files(path, track):
+	"""Given a new directroy, prompts the user to identify the audio and lyric files in the folder, and returns them and their file types"""
+	#Walks through the directory and uses the track name to try to find potential files related to the song
 	identifiers = []
 	for word in track.split():
 		identifiers.append(word)
@@ -51,81 +72,90 @@ def find_files(path, track):
 	potential_files = []
 	for file_name in file_list:
 		for word in identifiers:
-			if word.lower() in file_name.lower() and file_name not in potential_files:
+			if word.lower() in file_name.lower() and file_name not in potential_files: #makes it not case-sensitive, and avoids adding the same file more than once.
 				potential_files.append(file_name)
-	if len(potential_files) == 0:
+	if len(potential_files) == 0: #if none of the files look right, it assumes all of them are potentially right
 		potential_files = file_list
-	print potential_files
-	choice = raw_input('Which of these files is the song? Please enter its index number, starting from 0. If the song is not in this directory, enter "none".')
-	if choice == 'none':
+	potential_files.append('None of the above')
+	#display potential files for the user and ask them to choose
+	display_files = [str(i+1)+': '+potential_files[i] for i in range(len(potential_files))]
+	for file_name in display_files:
+		print file_name
+	choice = file_choice(display_files, 'song')
+	#if they choose none of the above, give them the option to change directories
+	if choice == None:
 		path = find_directory()
 		return find_files(path, track)
-	else:
-		choice = int(choice)
-		choice2 = int(raw_input('Which contains the lyrics?'))
+	#repeat for lyrics
+	choice2 = file_choice(display_files, 'lyrics')
+	if choice2 == None:
+		path = find_directory()
+		return find_files(path, track)
+	#create full file paths
 	song_path = path + '/' + potential_files[choice]
 	lyric_path = path + '/' + potential_files[choice2]
+	#figure out file type
 	file_type_list1 = potential_files[choice].split('.')
 	file_type_list2 = potential_files[choice2].split('.')
 	return [song_path, lyric_path, file_type_list1[1], file_type_list2[1]]
 
-
-track = raw_input('What is the name of the song?')
-artist = raw_input('What is the name of the artist?')
-folder_name = track.replace(' ','_') + '_'+artist.replace(' ','_')
-if os.path.exists(folder_name) and os.path.isdir(folder_name):
-	os.chdir(folder_name)
-	path = os.getcwd()
-	new_song_path = path + '/' + folder_name + '.mp3'
-	new_lyric_path = path + '/' + folder_name + '.txt'
-	if not os.path.exists(new_song_path) or os.path.exists(new_lyric_path):
-		print "I'm having trouble finding the required files."
-		new_path = os.getcwd()
-		[song_path, lyric_path, file_type_music, file_type_lyrics] = find_files(path, track)
-		new_song_path = new_path + '/' + folder_name + '.' + file_type_music
-		new_lyric_path = new_path + '/' + folder_name + '.'+ file_type_lyrics
-		os.rename(song_path, new_song_path)
-		os.rename(lyric_path, new_lyric_path)
-else:
-	os.makedirs(folder_name)
-	os.chdir(folder_name)
-	new_path = os.getcwd()
-	path = find_directory()
-	[song_path, lyric_path, file_type_music, file_type_lyrics] = find_files(path, track)
+def move_files(current_path, new_path, folder_name, track):
+	"""moves an audio and a lyric file from a given location to a new one, names them appropriately, and preseves the file type"""
+	[song_path, lyric_path, file_type_music, file_type_lyrics] = find_files(current_path, track)
 	new_song_path = new_path + '/' + folder_name + '.' + file_type_music
 	new_lyric_path = new_path + '/' + folder_name + '.'+ file_type_lyrics
 	os.rename(song_path, new_song_path)
 	os.rename(lyric_path, new_lyric_path)
+	return [new_song_path, new_lyric_path]
 
-
-pickle_name = folder_name+'.pkl'
-if not os.path.exists(pickle_name): #this is fake. it's a standin for if the song has already been done and pickled TODO: make this not fake
-	song = audio.Song(new_song_path, new_lyric_path,track,artist)
-	run_analyses(song)
-	pkl_file = open(pickle_name, 'wb')
-	pickle.dump(song, pkl_file)
-	pkl_file.close()
-else:
-	pkl_file = open(pickle_name, 'rb')
-	song = pickle.load(pkl_file)
-	print song
-	pkl_file.close()
-
-
-
-
-pygame.init()
-#select screen size
-size= (950, 650)
-model= squiggle.Model(song, size[0], size[1]) #TODO: pass song to model
-view= squiggle.View(model, size)
-running= True
-#checks if the user closes the window   
-while running:
-    for event in pygame.event.get():
-        if event.type== QUIT:
-            running= False
-    #if the window is open, do these things
-    model.update()
-    view.draw()
-    time.sleep(.001)
+if __name__ == '__main__':
+	#prompt for song details
+	track = raw_input('What is the name of the song?')
+	artist = raw_input('What is the name of the artist?')
+	folder_name = track.replace(' ','_') + '_'+artist.replace(' ','_')
+	#check if song folder exists, and mp3 and txt files are in it
+	if os.path.exists(folder_name) and os.path.isdir(folder_name):
+		os.chdir(folder_name)
+		path = os.getcwd()
+		song_path = path + '/' + folder_name + '.mp3'
+		lyric_path = path + '/' + folder_name + '.txt'
+		if not os.path.exists(song_path) or os.path.exists(lyric_path):
+			print "I'm having trouble finding the required files."
+			new_path = os.getcwd()
+			path = find_directory()
+			[new_song_path, new_lyric_path] = move_files(path, new_path, folder_name, track)
+	#if not, go through file-finding process
+	else:
+		os.makedirs(folder_name)
+		os.chdir(folder_name)
+		new_path = os.getcwd()
+		path = find_directory()
+		[new_song_path, new_lyric_path] = move_files(path, new_path, folder_name, track)
+	#check if the song has been pickled
+	pickle_name = folder_name+'.pkl'
+	if not os.path.exists(pickle_name):
+		song = audio.Song(new_song_path, new_lyric_path,track,artist) #create song object
+		run_analyses(song)
+		pkl_file = open(pickle_name, 'wb')
+		pickle.dump(song, pkl_file)
+		pkl_file.close()
+	#if it has been pickled, load
+	else:
+		pkl_file = open(pickle_name, 'rb')
+		song = pickle.load(pkl_file)
+		pkl_file.close()
+	#start pygame
+	pygame.init()
+	size= (950, 650)
+	model= squiggle.Model(song, size[0], size[1])
+	view= squiggle.View(model, size)
+	running= True
+	#checks if the user closes the window   
+	while running:
+	    for event in pygame.event.get():
+	        if event.type== QUIT:
+	            running= False
+	    #if the window is open, do these things
+	    model.update()
+	    view.draw()
+	    time.sleep(.001)
